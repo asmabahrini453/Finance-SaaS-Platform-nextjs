b6b0ab1c554cea61970a22c422afe22403d108fe
asma <bahriniasma453@gmail.com>
2024-11-14 10:55:24 +0100

    Create Summary APIs & Hook

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -6,6 +6,7 @@ import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import accounts from "./accounts"
 import categories from "./categories"
 import transactions from "./transactions";
+import summary from "./summary";
 
 
 export const runtime = 'edge'
@@ -17,6 +18,7 @@ const routes = app
   .route("/accounts", accounts)
   .route("/categories", categories)
   .route("/transactions", transactions)
+  .route("/summary", summary);
 
 export const GET = handle(app);
 export const POST = handle(app);
